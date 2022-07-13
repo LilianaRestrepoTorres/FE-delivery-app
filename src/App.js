495e1a352a889f09f59853fea7f29eaf53d4f100
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-07-13 01:43:14 -0500

    Getting data from local API and rendering it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [deliveries, setDeliveries] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:9393/deliveries")
+    .then((response) => {
+        console.log(response.data);
+        setDeliveries(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Deliveries App!</h1>
+        {deliveries.map((delivery)=>{
+          return(
+            <div key={delivery.id}>
+              <h3>Id: {delivery.id}</h3>
+              <p>Date: {delivery.creation_date}</p>
+              <p>State: {delivery.state}</p>
+              <p>Zone: {delivery.zone_id}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
